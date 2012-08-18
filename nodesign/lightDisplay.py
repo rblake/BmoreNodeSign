fc72f563a4846ee5563216f55a378ff43c33bb0d
Robert Blake <rob.c.blake.3@gmail.com>
2012-08-18 08:10:39 -0400

    Adding in a new server/client interface.  Implements round robin scheduling.

---
+++ nodesign/lightDisplay.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+
+import os.path
+
+from twisted.protocols.basic import NetstringReceiver
+from twisted.internet.protocol import Protocol,Factory
+from twisted.internet.task import LoopingCall
+from twisted.internet import reactor, defer
+from twisted.internet.task import deferLater
+
+import collections
+import pygame
+import numpy as np
+import write_image
+import re
+
+def d_wait(sec):
+    #helper to wait for a number of seconds.
+    return deferLater(reactor, sec, lambda: None)
+
+_acquire_req = "GIMME THE DISPLAY\n"
+_acquire_ack = "YOUR TURN\n"
+_display_req = "DISPLAY\n"
+_display_ack = "OK\n"
+_release_req = "DONE WITH THE DISPLAY\n"
+
+class LightDisplayClient(NetstringReceiver):
+    def __init__(self):
+        self._frameBuffer = collections.deque()
+        self._accessQueue = collections.deque()
+
+    def d_size(self):
+        return self._sizeDeferred
+
+    def d_acquire(self):
+        d = defer.Deferred()
+        self._accessQueue.append(d)
+        self.sendString(_acquire_req)
+        return d
+
+    def d_waitForDisplay(self):
+        return self._displayFinished
+
+    def release(self):
+        self.sendString(_release_req)
+
+    def d_display(self, frame):
+        if not self._frameBuffer:
+            self._displayFinished = defer.Deferred()
+        d = defer.Deferred()
+        arr = pygame.surfarray.array3d(frame)
+        stringified_frame = arr.tostring()
+        self.sendString(_display_req+stringified_frame)
+        self._frameBuffer.append(d)
+
+    def connectionMade(self):
+        NetstringReceiver.connectionMade(self)
+        self._hasSize = False
+        self._sizeDeferred = defer.Deferred()
+
+    def stringReceived(self, frame):
+        print "CLIENT_BEGIN", frame, "CLIENT_END"
+        if not self._sizeDeferred.called:
+            m = re.match(r'WIDTH (\d+) HEIGHT (\d+)\n', frame)
+            if m:
+                self._width = int(m.group(1))
+                self._height = int(m.group(2))
+                self._sizeDeferred.callback((self._width, self._height))
+        elif frame==_acquire_ack:
+            if self._accessQueue:
+                self._accessQueue.popleft().callback(None)
+        elif frame==_display_ack:
+            if self._frameBuffer:
+                self._frameBuffer.popleft().callback(None)
+            if not self._frameBuffer:
+                self._displayFinished.callback(None)
+        else:
+            #error!
+            pass
+
+class LightDisplayServer(NetstringReceiver):
+    def __init__(self, lightDisplay):
+        self._lightDisplay = lightDisplay
+
+    def connectionMade(self):
+        NetstringReceiver.connectionMade(self)
+        self.sendString("WIDTH %d HEIGHT %d\n" % (self._lightDisplay.width, self._lightDisplay.height))
+
+    def stringReceived(self, data):
+        print "SERVER_BEGIN",data[0:3], len(data),"SERVER_END"
+        if data==_acquire_req:
+            d = self._lightDisplay.d_acquire()
+            d.addCallback(self.acquireCallback)
+        elif data==_release_req:
+            self._lightDisplay.release()
+        elif data[0:len(_display_req)]==_display_req:
+            frame = data[len(_display_req):]
+            self._lightDisplay.d_display(frame).addCallback(self.displayCallback)
+
+    def acquireCallback(self, result):
+        self.sendString(_acquire_ack)
+
+    def displayCallback(self, result):
+        self.sendString(_display_ack)
+
+class _BaseLightDisplay:
+    def __init__(self, fps, dims):
+        self.width, self.height = dims
+        self._fps = fps
+        self._frameBuffer = collections.deque()
+        self._accessQueue = collections.deque()
+        self._loopingCall = LoopingCall(self._displayNext)
+        self._loopingCall.start(1.0/ self._fps)
+
+    def d_acquire(self):
+        d = defer.Deferred()
+        self._accessQueue.append(d)
+        if len(self._accessQueue)==1:
+            d.callback(None)
+        return d
+
+    def release(self):
+        self._accessQueue.popleft()
+        if self._accessQueue:
+            self._displayFinished.chainDeferred(self.accessQueue[0])
+
+    def _displayNext(self):
+        if self._frameBuffer:
+            #display the client frame.
+            frame, d = self._frameBuffer.popleft()
+            self._displayNow(frame)
+            d.callback(None)
+            if not self._frameBuffer:
+                self._displayFinished.callback(None)
+
+    def d_display(self, frame):
+        if not self._frameBuffer:
+            self._displayFinished = defer.Deferred()
+        d = defer.Deferred()
+        self._frameBuffer.append((frame, d))
+        return d
+
+class InMemoryDisplay(_BaseLightDisplay):
+    def __init__(self, pixelSize, fps, dims=(64,8)):
+        _BaseLightDisplay.__init__(self, fps, dims)
+        self.screen = pygame.display.set_mode(
+            (self.width * pixelSize, self.height * pixelSize)
+        )
+        self.smallScreen = pygame.Surface((self.width,self.height))
+        
+    def _displayNow(self, frame):
+        thisFrame = np.reshape(
+            np.fromstring(
+                frame,
+                dtype=np.uint8,
+                count=self.width * self.height * 3),
+            (self.width, self.height, 3)
+        )
+
+        pygame.surfarray.blit_array(self.smallScreen, thisFrame)
+        pygame.transform.scale(
+            self.smallScreen,
+            self.screen.get_size(),
+            self.screen
+        )
+        pygame.display.flip()
+        
+
+class SingleSignDisplay(_BaseLightDisplay,Protocol):
+    def __init__(self, fps, dims=(64,8)):
+        _BaseLightDisplay.__init__(self, fps, dims)
+        Protocol.__init__(self)
+        serial_port = write_image.find_arduino()
+        SerialPort(USBClient(), serial_port, reactor, baudrate='19200')
+
+    def _displayNow(self, frame):
+        self.transport.write(write_image.convert_frame_for_arduino(frame))
+
+
+class LightDisplayServerFactory(Factory):
+    def __init__(self, lightDisplay):
+        self._lightDisplay = lightDisplay
+
+    def buildProtocol(self, addr):
+        return LightDisplayServer(self._lightDisplay)
+
+class LightDisplayClientFactory(Factory):
+    def buildProtocol(self, addr):
+        return LightDisplayClient()
+
+if __name__=="__main__":
+    lightDisplay = InMemoryDisplay(16,30)
+    reactor.listenTCP(1234, LightDisplayServerFactory(lightDisplay))
+    reactor.run()
---
+++ nodesign/triviaGame.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+
+import pygame
+
+from lightDisplay import LightDisplayClientFactory, d_wait
+from twisted.internet import reactor, defer
+from twisted.internet.endpoints import TCP4ClientEndpoint
+
+class TextRenderer:
+    def __init__(self):
+        self.font = pygame.image.load("8x8font.png")
+    
+    def render_text_bitmap(self, text):
+        rendered_text = pygame.Surface((len(text)*8,8))
+        for ii in xrange(0,len(text)):
+            rendered_text.blit(self.font, (ii*8,0), area=pygame.Rect(ord(text[ii])*8,0,8,8))
+        return rendered_text
+
+class Effects:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.surface = pygame.Surface((width,height))
+
+    def copy_display(self):
+        return self.surface.copy()
+
+    def fill_screen(self,color):
+        frame = self.copy_display()
+        frame.fill(color)
+        yield frame
+
+    def marquee(self,bitmap,direction="left",begin="right",end="left"):
+        bbbb = self.copy_display()
+        if direction=="left":
+            pos_increment = -1
+        else:
+            pos_increment = 1
+        
+        if begin=="left":
+            begin_pos = 0
+        else:
+            begin_pos = bbbb.get_width()
+        if end=="left":
+            end_pos = -bitmap.get_width() 
+        else:
+            end_pos = bbbb.get_width()-bitmap.get_width()
+            
+        pos = begin_pos
+        while 1:
+            frame = bbbb.copy()
+            frame.blit(bitmap, (pos,0))
+            yield frame
+            pos += pos_increment
+            if pos*pos_increment >= end_pos*pos_increment:
+                break
+
+
+
+class TriviaQuestion:
+    def __init__(self,question, answer, choices=[]):
+        self.question = question
+        self.answer = answer
+        self.choices = choices
+
+
+@defer.inlineCallbacks
+def trivia_game(d_lights):
+    lights = yield d_lights
+    database = [TriviaQuestion("The answer is A", "A", ["Asadfsfdsd ", "Badsfsadfdsfs", "Casdfsadfdsf"]),
+                TriviaQuestion("Pick 2", "2", ["1", "2", "3"]),
+                ]
+    tq_index = 0
+    width, height = yield lights.d_size()
+    background = (255,255,255)
+    e = Effects(width,height)
+    ttt = TextRenderer()
+    while True:
+        yield lights.d_acquire()
+        for f in e.fill_screen(background): lights.d_display(f)
+        tq = database[tq_index]
+        for f in e.marquee(ttt.render_text_bitmap(tq.question), direction="left",begin="right",end="left"): lights.d_display(f)
+
+        for f in e.marquee(ttt.render_text_bitmap(tq.question), direction="left",begin="right",end="left"): lights.d_display(f)
+        yield lights.d_waitForDisplay()
+        for choice in tq.choices:
+            for f in e.fill_screen(background): lights.d_display(f)
+            first = 1
+            for f in e.marquee(ttt.render_text_bitmap(choice),direction="left",begin="left",end="right"):
+                lights.d_display(f)
+                if first:
+                    yield lights.d_waitForDisplay()
+                    yield d_wait(0.5)
+                first = 0
+            yield lights.d_waitForDisplay()
+            yield d_wait(0.5)
+        first = 1
+        for f in e.fill_screen(background): lights.d_display(f)
+        for f in e.marquee(ttt.render_text_bitmap("Answer: "+tq.answer),direction="left",begin="left",end="left"):
+            lights.d_display(f)
+            if first:
+                yield lights.d_waitForDisplay()
+                yield d_wait(0.5)
+            first = 0
+        yield lights.d_waitForDisplay()
+        yield d_wait(0.5)
+        lights.release()
+        tq_index += 1
+        if tq_index >= len(database):
+            tq_index = 0
+        
+
+if __name__=="__main__":
+    point = TCP4ClientEndpoint(reactor, "localhost", 1234)
+    trivia_game(point.connect(LightDisplayClientFactory()))
+    reactor.run()
