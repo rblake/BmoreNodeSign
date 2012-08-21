87a4dfffc5c2326b60115c130d9f2d70b6a84722
Robert Blake <rob.c.blake.3@gmail.com>
2012-08-21 00:16:42 -0400

    Adding code to display a single frame.

---
+++ nodesign/singleFrame.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import pygame
+
+from lightDisplay import LightDisplayClientFactory, d_wait
+from twisted.internet import reactor, defer
+from twisted.internet.endpoints import TCP4ClientEndpoint
+
+@defer.inlineCallbacks
+def singleFrame(d_lights, image):
+    lights = yield d_lights
+    width, height = yield lights.d_size()
+    yield lights.d_acquire()
+    f = pygame.image.load(image)
+    yield lights.d_display(f)
+    lights.release()
+
+if __name__=="__main__":
+    import sys
+
+    point = TCP4ClientEndpoint(reactor, "localhost", 1234)
+    singleFrame(point.connect(LightDisplayClientFactory()), sys.argv[1])
+    reactor.run()
