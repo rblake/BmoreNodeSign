06059b586a87b266d91dbcdb74ee7b0a24f8d86f
Robert Blake <rob.c.blake.3@gmail.com>
2012-08-22 22:05:32 -0400

    Making rainbow prettier & less jittery

--- nodesign/rainbow.py
+++ nodesign/rainbow.py
@@ -19,18 +19,18 @@ def rainbow(d_lights):
     while True:
         #start rendering a cycle.
         yield lights.d_acquire()
-        for p in xrange(0, 360*5, 5):
+        for p in xrange(0, 360*7, 5):
             for x in xrange(0, width):
                 for y in xrange(0, height):
                     p_rad = p/180.*3.14159
-                    hue = (2+math.sin(float(x)/width+p_rad+0.5)+math.sin((float(y)/height*1.2+p_rad)*1.2))/4
+                    hue = (2+math.sin(float(x)/width-p_rad/7+0.5)+math.sin(float(y)/height-p_rad))/4
                     (r,g,b) = colorsys.hsv_to_rgb(hue,1,1)
                     pixels[x,y,0] = int(255*r)
                     pixels[x,y,1] = int(255*g)
                     pixels[x,y,2] = int(255*b)
             pygame.surfarray.blit_array(surf,pixels)
             lights.d_display(surf)
-            yield d_wait(0)
+            yield d_wait(0) #this returns control to twisted temporarily.
         lights.release()
 
 if __name__=="__main__":
