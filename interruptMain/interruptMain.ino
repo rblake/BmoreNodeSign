f2efc1d84f02a85509f4a2239edb68bf4a185990
Robert Blake <rob.c.blake.3@gmail.com>
2014-11-15 12:57:59 -0500

    Registers are now being written correctly.  Check pinmaps excel for pinout.

--- interruptMain/interruptMain.ino
+++ interruptMain/interruptMain.ino
@@ -7,37 +7,50 @@
 #define BITS_PER_COLOR 12
 #define BYTES_PER_FRAME (BITS_PER_COLOR*BYTES_PER_OUTPUT)
 
+#define BMASK (0x063FF000)
+#define CMASK (0x37EFF3FE)
+#define DMASK (0x000003FF)
+
+#define OUTPUT_MASKS() do {                     \
+REG_PIOB_PER = REG_PIOB_OER = BMASK;            \
+REG_PIOC_PER = REG_PIOC_OER = CMASK;            \
+REG_PIOD_PER = REG_PIOD_OER = DMASK;            \
+} while (0);
 
 //FIXME!  Write zero to all the registers
-#define SAY_LOW do {                            \
-PORTA = 0x00;                                   \
-PORTB = 0x00;                                   \
-PORTC = 0x00;                                   \
-PORTE = 0x00;                                   \
-PORTF = 0x00;                                   \
-PORTG = 0x00;                                   \
+#define SAY_LOW() do {                          \
+REG_PIOB_ODSR = 0;                              \
+REG_PIOC_ODSR = 0;                              \
+REG_PIOD_ODSR = 0;                              \
 } while(0)
 
 //FIXME! Write one to all the registers
-#define SAY_HIGH do {                           \
-PORTA = 0xFF;                                   \
-PORTB = 0xFF;                                   \
-PORTC = 0xFF;                                   \
-PORTE = 0xFF;                                   \
-PORTF = 0xFF;                                   \
-PORTG = 0xFF;                                   \
+#define SAY_HIGH() do {                         \
+REG_PIOB_ODSR = 0xFFFFFFFF;                     \
+REG_PIOC_ODSR = 0xFFFFFFFF;                     \
+REG_PIOD_ODSR = 0xFFFFFFFF;                     \
 } while(0)
 
 //FIXME: write out 6 bytes to all the registers, based off of the pin
 //mapping.
-#define SAY_VAR do {                           \
-PORTA = 0xFF;                                   \
-PORTB = 0xFF;                                   \
-PORTC = 0xFF;                                   \
-PORTE = 0xFF;                                   \
-PORTF = 0xFF;                                   \
-PORTG = 0xFF;                                   \
-} while(0)
+inline SAY_VAR(byte out[6]) {
+  uint32 AAA=out[0];
+  uint32 BBB=out[1];
+  uint32 CCC=out[2];
+  uint32 DDD=out[3];
+  uint32 EEE=out[4];
+  uint32 FFF=out[5];
+  uint32 midFFF = FFF&0x1C;
+
+  REG_PIOB_ODSR = ((AAA << 14) | (BBB << 6) | (FFF << 25)) & BMASK;
+  REG_PIOC_ODSR = ( ((BBB&0x3F) << 21) 
+                   | (CCC << 12) 
+                   | (DDD << 2) 
+                   | (midFFF >> 3)
+                   | (midFFF << 26)
+                   ) & CMASK;
+  REG_PIOD_ODSR = ((EEE << 3) | (FFF >> 5)) & DMASK;
+}
 
 byte displayA[BYTES_PER_FRAME*LIGHTS_PER_STRING+1] =
 "\xfe\xff\xff\xfe\xff\xff\xfe\xff\xff\xfe\xff\xff\xfe\xff\xff\xfe\xff\xff\xfe\xff\xff\xfe\xff\xff"
@@ -132,8 +145,8 @@ volatile byte addr;
 byte intensity=0xCC;
 volatile byte* outputBuffer;
 volatile int iterCount=WAIT_FOR_FRAME;
-
 #if 0
+//#define allLightsInterrupt TC0_Handler 
 void allLightsInterrupt() {
   
   //Trigger that we're sending now.
@@ -147,20 +160,20 @@ void allLightsInterrupt() {
     default:
       int bit = (iterCount-2)/3;
       switch (bit) {
-      case  0: if (addr & B100000) {SAY_LOW;} else {SAY_HIGH;} break;
-      case  1: if (addr & B010000) {SAY_LOW;} else {SAY_HIGH;} break;
-      case  2: if (addr & B001000) {SAY_LOW;} else {SAY_HIGH;} break;
-      case  3: if (addr & B000100) {SAY_LOW;} else {SAY_HIGH;} break;
-      case  4: if (addr & B000010) {SAY_LOW;} else {SAY_HIGH;} break;
-      case  5: if (addr & B000001) {SAY_LOW;} else {SAY_HIGH;} break;
-      case  6: if (intensity & B10000000) {SAY_LOW;} else {SAY_HIGH;} break;
-      case  7: if (intensity & B01000000) {SAY_LOW;} else {SAY_HIGH;} break;
-      case  8: if (intensity & B00100000) {SAY_LOW;} else {SAY_HIGH;} break;
-      case  9: if (intensity & B00010000) {SAY_LOW;} else {SAY_HIGH;} break;
-      case 10: if (intensity & B00001000) {SAY_LOW;} else {SAY_HIGH;} break;
-      case 11: if (intensity & B00000100) {SAY_LOW;} else {SAY_HIGH;} break;
-      case 12: if (intensity & B00000010) {SAY_LOW;} else {SAY_HIGH;} break;
-      case 13: if (intensity & B00000001) {SAY_LOW;} else {SAY_HIGH;} break;
+      case  0: if (addr & B100000) {SAY_LOW();} else {SAY_HIGH();} break;
+      case  1: if (addr & B010000) {SAY_LOW();} else {SAY_HIGH();} break;
+      case  2: if (addr & B001000) {SAY_LOW();} else {SAY_HIGH();} break;
+      case  3: if (addr & B000100) {SAY_LOW();} else {SAY_HIGH();} break;
+      case  4: if (addr & B000010) {SAY_LOW();} else {SAY_HIGH();} break;
+      case  5: if (addr & B000001) {SAY_LOW();} else {SAY_HIGH();} break;
+      case  6: if (intensity & B10000000) {SAY_LOW();} else {SAY_HIGH();} break;
+      case  7: if (intensity & B01000000) {SAY_LOW();} else {SAY_HIGH();} break;
+      case  8: if (intensity & B00100000) {SAY_LOW();} else {SAY_HIGH();} break;
+      case  9: if (intensity & B00010000) {SAY_LOW();} else {SAY_HIGH();} break;
+      case 10: if (intensity & B00001000) {SAY_LOW();} else {SAY_HIGH();} break;
+      case 11: if (intensity & B00000100) {SAY_LOW();} else {SAY_HIGH();} break;
+      case 12: if (intensity & B00000010) {SAY_LOW();} else {SAY_HIGH();} break;
+      case 13: if (intensity & B00000001) {SAY_LOW();} else {SAY_HIGH();} break;
       default:
         SAY_VAR(outBuffer);
         outBuffer += BYTES_PER_OUTPUT;
@@ -195,8 +208,8 @@ void renderFrame() {
 }
 
 volatile boolean l;
- 
-void TC0_Handler()
+#define blink TC0_Handler 
+void blink()
 {
     long dummy=REG_TC0_SR0; // vital - reading this clears some flag
                             // otherwise you get infinite interrupts
@@ -204,10 +217,15 @@ void TC0_Handler()
     digitalWrite(13,l);
 }
 
+const bool blink = true;
+
 void setup() 
 {
-  //Serial.println(ARDUINO_ID);
-  pinMode(13,OUTPUT);
+  if (blink) {
+    pinMode(13,OUTPUT);
+  } else {
+    Serial.println(ARDUINO_ID);
+  }
   
   //setup interrupts
   pmc_enable_periph_clk(ID_TC0);
@@ -238,33 +256,36 @@ void setup()
 
 void loop()
 {
-  return;
-  while (1) {}
-  
-  do {
-    renderFrame();
-    const bool do_read = false;
-    if (do_read) {
-      Serial.println("OK");
-      //search for an "OK"
-      bool found_ok = false;
-      while (!found_ok) {
-        while (Serial.available() < 2) {}
-        byte O = Serial.read();
-        if (O == 'O') {
-          byte K = Serial.read();
-          if (K == 'K') {
-            found_ok = true;
-          }
+  if (blink) {
+    return;
+  }
+
+  renderFrame();
+  const bool do_read = false;
+  if (do_read) {
+    Serial.println("OK");
+    //search for an "OK"
+    bool found_ok = false;
+    while (!found_ok) {
+      while (Serial.available() < 2) {}
+      byte O = Serial.read();
+      if (O == 'O') {
+        byte K = Serial.read();
+        if (K == 'K') {
+          found_ok = true;
         }
       }
-      for (int ii=0; ii<LIGHTS_PER_STRING; ii++) {
-        for (int jj=0; jj<BYTES_PER_FRAME; jj++) {
-          while (Serial.available() < 1) {}
-          nextDisplay[ii*BYTES_PER_FRAME+jj]=Serial.read();
-        }
-      }
-      //swap(currentDisplay,nextDisplay);
     }
-  } while (1);
+    for (int ii=0; ii<LIGHTS_PER_STRING; ii++) {
+      for (int jj=0; jj<BYTES_PER_FRAME; jj++) {
+        while (Serial.available() < 1) {}
+        nextDisplay[ii*BYTES_PER_FRAME+jj]=Serial.read();
+      }
+    }
+    //swap the displays
+    //swap(currentDisplay,nextDisplay);
+    byte* temp = currentDisplay;
+    currentDisplay = nextDisplay;
+    nextDisplay = temp;
+  }
 }
